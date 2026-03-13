edb4bf142577c4d6fada86ebe850d27fc617c80d
Egin Pardewan <eginpardewan@gmail.com>
2026-03-13 15:46:56 +0700

    add database connection variables

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -2,3 +2,21 @@
 using System;
 using System.Windows.Forms;
 
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection conn;
+        MySqlCommand cmd;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
